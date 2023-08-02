1651f4ca2fb23da01f0b7faa0fef16017085f214
corynko <ethankotel@gmail.com>
2023-08-01 19:38:16 -0600

    built out src-sw

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,9 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: "development",
@@ -20,11 +23,13 @@ module.exports = () => {
         title: "Editor",
       }),
 
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
 
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
